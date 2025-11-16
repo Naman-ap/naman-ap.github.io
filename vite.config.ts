b8abce34cac352fe256589c47c5fc406d3e46b17
Naman-ap <141435865+Naman-ap@users.noreply.github.com>
2025-11-16 20:22:09 +0530

    Update build script in package.json to include TypeScript compilation and set base URL in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/Naman-ap/Portfolio/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
